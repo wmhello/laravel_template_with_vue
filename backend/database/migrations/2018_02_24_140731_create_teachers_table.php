8269b6a8e497f150c5fc55db1c0b27c7dca11f68
wmhello <871228582@qq.com>
2018-02-26 12:15:18 +0800

    修复补丁 表中增加注释造成的无法迁移的补丁

--- backend/database/migrations/2018_02_24_140731_create_teachers_table.php
+++ backend/database/migrations/2018_02_24_140731_create_teachers_table.php
@@ -30,12 +30,12 @@ class CreateTeachersTable extends Migration
             $table->unsignedTinyInteger('state')->comment('人员状态(1->正常 2->调出 3->退出 4->停薪留职)')->default(1);
             $table->unsignedTinyInteger('teaching_id')->comment('教学科目Id')->nullable;
             $table->timestamps();
-            $table->comment('教职工信息表');
             $table->unique('card_id');
             $table->unique('phone');
             $table->unique('phone2');
-            $table->foreign('teaching_id')->reference('id')->on('yz_teaching');
-            $table->foreign('nation_id')->reference('id')->on('yz_nation');
+            // 要设置外键的话  必须先保证相应的表格先存在
+            // $table->foreign('teaching_id')->reference('id')->on('yz_teaching');
+            // $table->foreign('nation_id')->reference('id')->on('yz_nation');
         });
     }
 
