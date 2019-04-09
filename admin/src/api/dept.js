12cd91388e3c71daeb0efe799517f36349711943
wmhello <871228582@qq.com>
2019-04-09 22:29:46 +0800

    文档更新

--- admin/src/api/dept.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchDeptTree(_params) {
-  return request({
-    url: '/admin/dept/tree',
-    method: 'get',
-    params: _params
-  })
-}
--- admin/src/api/menu.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchUserTree() {
-  return request({
-    url: '/admin/api/userTree',
-    method: 'get'
-  })
-}
